373538aa3f8ce76863981b1a33ac88ea8af5275a
dominik.kopczynski <dominik.kopczynski@isas.de>
2015-11-04 14:24:12 +0100

    python access changes

--- get-msdata.py
+++ get-msdata.py
@@ -1,31 +1,33 @@
+#!/home/dominik.kopczynski/anaconda3/bin/python3.5
+
 import sqlite3
 import zlib
 import struct
 import json
+import cgi, cgitb
+
+print("Content-Type: text/html")
+print()
+
+form = cgi.FieldStorage()
+proteins = form.getvalue('proteins')
 
 def make_dict(cur):
     return {key[0]: value for key, value in zip(cur.description, cur.fetchall()[0])}
 
-def index(req):
-    spectrum_id = 1
-    split_args = req.args.split("&")
-    for arg in split_args:
-        token = arg.split("=")
-        if token[0] == "spectrum_id":
-            spectrum_id = token[1]
-            break
-        
-    db = sqlite3.connect('/home/dominik.kopczynski/Data/blib/TestLibraryPS.blib')
-    cur = db.cursor()
-    cur.execute('SELECT * FROM RefSpectra r INNER JOIN RefSpectraPeaks p ON r.id = p.RefSpectraID WHERE r.id = %i;' % int(spectrum_id))
-    result = make_dict(cur)
+spectrum_id = int(form.getvalue('spectrum_id'))
     
-    try: result["peakMZ"] = zlib.decompress(result["peakMZ"])
-    except: pass
-    result["peakMZ"] = struct.unpack("%id" % (len(result["peakMZ"]) / 8), result["peakMZ"])
-    
-    try: result["peakIntensity"] = zlib.decompress(result["peakIntensity"])
-    except: pass
-    result["peakIntensity"] = struct.unpack("%if" % (len(result["peakIntensity"]) / 4), result["peakIntensity"])
-    
-    return json.dumps(result)
+db = sqlite3.connect('/home/dominik.kopczynski/Data/blib/TestLibraryPS.blib')
+cur = db.cursor()
+cur.execute('SELECT * FROM RefSpectra r INNER JOIN RefSpectraPeaks p ON r.id = p.RefSpectraID WHERE r.id = %i;' % int(spectrum_id))
+result = make_dict(cur)
+
+try: result["peakMZ"] = zlib.decompress(result["peakMZ"])
+except: pass
+result["peakMZ"] = struct.unpack("%id" % (len(result["peakMZ"]) / 8), result["peakMZ"])
+
+try: result["peakIntensity"] = zlib.decompress(result["peakIntensity"])
+except: pass
+result["peakIntensity"] = struct.unpack("%if" % (len(result["peakIntensity"]) / 4), result["peakIntensity"])
+
+print(json.dumps(result))
---
+++ ms-view.js
@@ -0,0 +1,414 @@
+var zoom = 0;
+var max_zoom = 30;
+var scaling = 1.2;
+var origin_x = 0;
+var last_x = 0;
+var left_border = 0;
+var right_border = 0;
+var top_border = 0;
+var bottom_border = 0;
+var x_tics = [200, 100, 50, 25, 10, 5, 2, 1];
+var y_tics = [0.2, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5];
+var peaks = new Array();
+var acids = new Array();
+var tolerance = 10;
+var H =       1.007276;
+var C12 =    12.000000;
+var O =      15.994914;
+var H3O =    19.016742;
+var O2 =     31.989829;
+var acetyl = 43.016742;
+var peptide = "";
+var charge = 0;
+var precursor_mass = 0;
+var grid_color = "#DDDDDD"
+var label_color = "black"
+var peak_color = "#888888"
+var peak_highlight_color = "#D40000"
+
+
+
+acids['A'] =  71.037110;
+acids['R'] = 156.101110;
+acids['N'] = 114.042930;
+acids['D'] = 115.026940;
+acids['C'] = 103.009190;
+acids['c'] = 160.030654;
+acids['E'] = 129.042590;
+acids['Q'] = 128.058580;
+acids['G'] =  57.021460;
+acids['H'] = 137.058910;
+acids['I'] = 113.084060;
+acids['L'] = 113.084060;
+acids['K'] = 128.094960;
+acids['M'] = 131.040490;
+acids['m'] = 147.035404;
+acids['n'] = 163.030318;
+acids['F'] = 147.068410;
+acids['P'] =  97.052760;
+acids['S'] =  87.032030;
+acids['T'] = 101.047680;
+acids['W'] = 186.079310;
+acids['Y'] = 163.063330;
+acids['V'] =  99.068410;
+
+
+
+function binary_search(key){
+    var low = 0;
+    var mid = 0;
+    var high = peaks.length - 1;
+    while (low <= high){
+        mid = (low + high) >> 1;
+        if (peaks[mid].mass == key) break;
+
+        else if (peaks[mid].mass < key) low = mid + 1;
+        else high = mid - 1;
+    }
+    var mn_index = mid;
+    var mn = Math.abs(peaks[mid].mass - key);
+    if (mid >= 1 && Math.abs(peaks[mid - 1].mass - key) < mn){
+        mn = Math.abs(peaks[mid - 1].mass - key);
+        mn_index = mid - 1;
+    }
+    if (mid + 1 < peaks.length && Math.abs(peaks[mid + 1].mass - key) < mn){
+        mn = Math.abs(peaks[mid + 1].mass - key);
+        mn_index = mid + 1;
+    }
+    return new Array(mn / key * 1000000, mn_index);
+}
+
+
+
+function peak(ms, xx, int){
+    this.mass = ms;
+    this.x = xx;
+    this.intensity = int;
+    this.annotation = "";
+    this.highlight = false;
+}
+
+
+
+function subscripting(x){
+    var str = "";
+    var num = 1;
+    var n = Math.floor(Math.log10(x)) + 1;
+    for (var i = 0; i < n; ++i){
+        str = String.fromCharCode(8320 + Math.floor(x % (num * 10) / num)) + str;
+        num *= 10;
+    }
+    return str;
+}
+
+
+
+function annotation(){
+    // annotate y-ions
+    var rev_peptide = peptide.split("").reverse().join("");
+    var mass = H3O;
+    for (var i = 0; i < rev_peptide.length; ++i){
+        mass += acids[rev_peptide[i]];
+        var diff_mass = binary_search(mass);
+        //console.log(diff_mass + " " + rev_peptide[i] + " " + mass + " " + peaks[diff_mass[1]].mass + " " + peaks[diff_mass[1]].intensity);
+        if (diff_mass[0] < tolerance){
+            peaks[diff_mass[1]].highlight = true;
+            peaks[diff_mass[1]].annotation = "y" + subscripting(i + 1);
+        }
+        else {
+            var diff_mass = binary_search(mass / 2);
+            //console.log(diff_mass + " " + rev_peptide[i] + " " + mass / 2, peaks[diff_mass[1]].mass);
+            if (diff_mass[0] < tolerance){
+                peaks[diff_mass[1]].highlight = true;
+                peaks[diff_mass[1]].annotation = "y" + subscripting(i + 1);
+            }
+        }
+    }
+    
+    
+    // annotate b-ions
+    var mass = H;
+    for (var i = 0; i < peptide.length; ++i){
+        mass += acids[peptide[i]];
+        var diff_mass = binary_search(mass);
+        //console.log(diff_mass + " " + rev_peptide[i] + " " + mass, peaks[diff_mass[1]].mass);
+        if (diff_mass[0] < tolerance){
+            peaks[diff_mass[1]].highlight = true;
+            peaks[diff_mass[1]].annotation = "b" + subscripting(i + 1);
+        }
+        else {
+            var diff_mass = binary_search(mass / 2);
+            //console.log(diff_mass + " " + rev_peptide[i] + " " + mass / 2, peaks[diff_mass[1]].mass);
+            if (diff_mass[0] < tolerance){
+                peaks[diff_mass[1]].highlight = true;
+                peaks[diff_mass[1]].annotation = "b" + subscripting(i + 1);
+            }
+        }
+    }
+}
+
+
+
+function init_view(){
+    var strGET = document.location.search.substr(1, document.location.search.length);
+    var HTTP_GET_VARS = new Array();
+    if(strGET != ''){
+        var gArr = strGET.split('&');
+        for(var i = 0; i < gArr.length; ++i){
+            var v = '';
+            var vArr = gArr[i].split('=');
+            if(vArr.length > 1){
+                v = vArr[1];
+            }
+            HTTP_GET_VARS[unescape(vArr[0])] = unescape(v);
+        }
+    }
+    var spectrum_id = (strGET != '' && HTTP_GET_VARS['spectrum_id']) ? HTTP_GET_VARS['spectrum_id'] : "1";
+    
+    var c = document.getElementById("msarea");
+    var ctx = c.getContext("2d");
+    ctx.canvas.width  = window.innerWidth * 0.8;
+    ctx.canvas.height = window.innerHeight * 0.8;
+    
+    var spectrum_data = 0;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            spectrum_data = JSON.parse(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", "get-msdata.py?spectrum_id=" + spectrum_id, false);
+    xmlhttp.send();
+    
+    peptide = spectrum_data["peptideSeq"];
+    precursor_mass = spectrum_data["precursorMZ"];
+    charge = spectrum_data["precursorCharge"];
+    
+    left_border = ctx.canvas.width * 0.05;
+    right_border = ctx.canvas.width * 0.95;
+    top_border = ctx.canvas.height * 0.05;
+    bottom_border = ctx.canvas.height * 0.95;
+    
+    peaks.push(new peak(0, 0, 0));
+    origin_x = left_border;
+    last_x = right_border;
+    var max_mass = 0;
+    for (var items in spectrum_data["peakMZ"]){
+        peaks.push(new peak(spectrum_data["peakMZ"][items], 0, 0));
+        max_mass = Math.max(max_mass, spectrum_data["peakMZ"][items]);
+    }
+    for (var i = 0; i < peaks.length; ++i) {
+        peaks[i].x = origin_x + ctx.canvas.width * 0.9 * peaks[i].mass / max_mass;
+    }
+    
+    var ii = 1;
+    console.log(spectrum_data["peakIntensity"].length);
+    for (var items in spectrum_data["peakIntensity"]){
+        peaks[ii].intensity = spectrum_data["peakIntensity"][items];
+        ii += 1;
+    }
+    
+    
+    c.addEventListener("mousewheel", mouse_wheel_listener, false);
+    c.addEventListener('DOMMouseScroll', mouse_wheel_listener, false);
+    annotation();
+    draw();
+}
+
+
+function draw(){
+    var c = document.getElementById("msarea");
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.font="10px Arial";
+    
+    
+    // Add x-axis tics, values and grid
+    var max_tic = peaks[peaks.length - 1].mass;
+    var tics_start = Math.min(peaks[0].x, left_border);
+    var tic = x_tics[Math.floor(zoom / 4)];
+    var width = Math.max(peaks[peaks.length - 1].x, right_border) - Math.min(peaks[0].x, left_border);
+    ctx.textAlign = "center";
+    ctx.textBaseline = 'middle';
+    for (var i = 0; i < max_tic; i += tic){
+        var x = tics_start + i * width / max_tic;
+        if (left_border <= x && x <= right_border){
+            
+            ctx.setLineDash([0]);
+            ctx.strokeStyle = label_color;
+            ctx.lineWidth = 2;
+            ctx.beginPath();
+            ctx.moveTo(x, bottom_border);
+            ctx.lineTo(x, bottom_border * 1.015);    
+            ctx.closePath();
+            ctx.stroke();
+            
+            var txt = i.toString();
+            ctx.fillText(txt, x, ctx.canvas.height * 0.975);
+            
+            
+            ctx.setLineDash([10]);
+            ctx.strokeStyle = grid_color;
+            ctx.lineWidth = 1;
+            ctx.beginPath();
+            ctx.moveTo(x, bottom_border);
+            ctx.lineTo(x, top_border);    
+            ctx.closePath();
+            ctx.stroke();
+        }
+    }
+    
+    var max_mass = 0;
+    var max_intensity = 0;
+    for (var i = 0; i < peaks.length; ++i) {
+        if (left_border <= peaks[i].x && peaks[i].x <= right_border){
+            max_intensity = Math.max(max_intensity, peaks[i].intensity);
+        }
+    }
+    
+    // Add y-axis tics, values and grid
+    var lg = Math.floor(Math.log10(max_intensity));
+    var y_tic = y_tics[Math.floor(max_intensity / Math.pow(10, lg))] * Math.pow(10, lg);
+    ctx.textAlign = "right";
+    ctx.textBaseline = 'middle';
+    for (var i = 0; i < max_intensity; i += y_tic){
+        var y = bottom_border - i * ctx.canvas.height * 0.85 / max_intensity;
+        ctx.setLineDash([0]);
+        ctx.strokeStyle = label_color;
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(left_border, y);
+        ctx.lineTo(left_border * 0.9, y);    
+        ctx.closePath();
+        ctx.stroke();
+        
+        var txt = i.toString();
+        ctx.fillText(txt, left_border * 0.86, y);
+        
+        ctx.setLineDash([10]);
+        ctx.strokeStyle = grid_color;
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(left_border, y);
+        ctx.lineTo(right_border, y);    
+        ctx.closePath();
+        ctx.stroke();
+    }
+    
+    
+    
+    // Add Masses
+    var zero_x = left_border;
+    var zero_y = bottom_border;
+    ctx.lineWidth = 1;
+    ctx.setLineDash([0]);
+    ctx.strokeStyle = peak_color;
+    var y_factor = ctx.canvas.height * 0.85 / max_intensity;
+    var y_offset = ctx.canvas.height * 0.02;
+    var annotated = new Array();
+    for (var i = 0; i < peaks.length; ++i) {
+        if (left_border <= peaks[i].x && peaks[i].x <= right_border){
+            if (!peaks[i].highlight){
+                ctx.beginPath();
+                ctx.moveTo(peaks[i].x, zero_y);
+                ctx.lineTo(peaks[i].x, zero_y - peaks[i].intensity * y_factor);    
+                ctx.closePath();
+                ctx.stroke();
+            }
+            else {
+                annotated.push(i);
+            }
+        }
+    }
+    
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = peak_highlight_color;
+    ctx.font="16px Arial";
+    for (var i = 0; i < annotated.length; ++i) {
+        ctx.beginPath();
+        ctx.moveTo(peaks[annotated[i]].x, zero_y);
+        ctx.lineTo(peaks[annotated[i]].x, zero_y - peaks[annotated[i]].intensity * y_factor);    
+        ctx.closePath();
+        ctx.stroke();
+        
+        ctx.textAlign = "center";
+        ctx.textBaseline = 'bottom';
+        ctx.fillText(peaks[annotated[i]].annotation, peaks[annotated[i]].x, zero_y - peaks[annotated[i]].intensity * y_factor - y_offset);
+    }
+    
+    
+    // Add axis and labels
+    ctx.strokeStyle = label_color;
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(zero_x, top_border);
+    ctx.lineTo(zero_x, zero_y);    
+    ctx.closePath();
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(zero_x, zero_y);    
+    ctx.lineTo(right_border, zero_y);    
+    ctx.closePath();
+    ctx.stroke();
+    ctx.font="16px Arial";
+    ctx.textAlign = "left";
+    ctx.textBaseline = 'top';
+    ctx.fillText("m/z", right_border * 1.005, bottom_border);
+    ctx.save();
+    ctx.translate(0, 0);
+    ctx.rotate(-Math.PI/2);
+    ctx.textAlign = "center";
+    ctx.fillText("intensity", left_border, top_border);
+    ctx.restore();
+}
+
+
+function get_mouse_pos(canvas, evt){
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
+
+
+function mouse_wheel_listener(event){
+    var direction = (1 - 2 *(event.detail >= 0));
+    if (zoom + direction < 0 || max_zoom <= zoom + direction)
+        return;
+    
+    zoom += direction;
+    var scale = scaling;
+    if (event.detail >= 0) scale = 1. / scale;
+    var c = document.getElementById("msarea");
+    var ctx = c.getContext("2d");
+    
+    res = get_mouse_pos(c, event);
+    origin_x = res.x + scale * (origin_x - res.x);
+    last_x = res.x + scale * (last_x - res.x);
+    for (var i = 0; i < peaks.length; ++i) {
+        peaks[i].x = res.x + scale * (peaks[i].x - res.x);
+    }
+    if (origin_x > ctx.canvas.width * 0.05){
+        var diff = origin_x - ctx.canvas.width * 0.05;
+        origin_x -= diff;
+        last_x -= diff;
+        for (var i = 0; i < peaks.length; ++i) {
+            peaks[i].x -= diff;
+        }
+    }
+    
+    if (last_x < ctx.canvas.width * 0.95){
+        var diff = ctx.canvas.width * 0.95 - last_x;
+        last_x += diff;
+        origin_x += diff;
+        for (var i = 0; i < peaks.length; ++i) {
+            peaks[i].x += diff;
+        }
+    }
+    
+    draw();
+}
+
+
+document.addEventListener('DOMContentLoaded', init_view, false);
