4718c04438970b9e8bdc68de65c9cf7f460c9abf
dominik.kopczynski <dominik.kopczynski@isas.de>
2016-02-09 15:10:31 +0100

    inserted Alanine, aspartate and glutamate metabolism

--- qsdb.js
+++ qsdb.js
@@ -79,7 +79,7 @@ infobox_offset_x = 20;
 preview_element = 0;
 
 
-pathways = [[50, "Pyrimidine metabolism"],
+pathways = [[15, "Alanine, aspartate and glutamate metabolism"],
             [163, "alpha-Linolenic acid metabolism"],
             [57, "Amino sugar and nucleotide sugar metabolism"],
             [160, "Arachidonic acid metabolism"],
@@ -101,6 +101,7 @@ pathways = [[50, "Pyrimidine metabolism"],
             [48, "Pentose and glucuronate interconversions"],
             [3, "Pentose phosphate"],
             [155, "Primary bile acid biosynthesis"],
+            [50, "Pyrimidine metabolism"],
             [49, "Purine metabolism"],
             [5, "Pyruvate metabolism"],
             [41, "Sphingolipid metabolism"],
