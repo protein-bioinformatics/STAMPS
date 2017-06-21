2187bcc5e26543e86fee8b63aa5d5cad49ed3314
dominik.kopczynski <dominik.kopczynski@isas.de>
2017-06-21 07:45:33 +0200

    preparing for loading chromosome data

--- cgi-bin/get-chromosome-data.py
+++ cgi-bin/get-chromosome-data.py
@@ -1,7 +1,8 @@
-#!/usr//bin/python3
+#!/home/dominik.kopczynski/anaconda3/bin/python3.5
 
 import json
 from pymysql import connect, cursors
+import cgi, cgitb
 
 print("Content-Type: text/html")
 print()
@@ -16,10 +17,17 @@ with open("../admin/qsdb.conf", mode="rt") as fl:
         conf[token[0].strip(" ")] = token[1].strip(" ")
 
 
+
+form = cgi.FieldStorage()
+chromosome = form.getvalue('chromosome')
+
+if chromosome.find("'") > -1 or chromosome.find("\"") > -1:
+    print(-1)
+    exit()
+
 conn = connect(host = conf["mysql_host"], port = int(conf["mysql_port"]), user = conf["mysql_user"], passwd = conf["mysql_passwd"], db = conf["mysql_db"])
-#my_cur = conn.cursor(cursors.DictCursor)
 my_cur = conn.cursor()
 
-my_cur.execute('SELECT fn.id, fn.name, fn.parent, count(pf.protein_id) cnt FROM function_names fn LEFT JOIN protein_functions pf ON fn.id = pf.function_id GROUP by fn.id, fn.name, fn.parent;')
-all_functions = [[row[0], row[1], row[2], row[3]] for row in my_cur]
-print(json.dumps(all_functions))
+my_cur.execute("SELECT id, name, definition, kegg_link, mass, accession, ec_number, chr_start, chr_end from proteins where chromosome = '%s' and unreviewed = false;" % chromosome)
+data = [row for row in my_cur]
+print(json.dumps(data))
