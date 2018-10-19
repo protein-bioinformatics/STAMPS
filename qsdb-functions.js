d4eca90d4d41e6a9df5292fe29ef5f8383080833
dominik.kopczynski <dominik.kopczynski@isas.de>
2018-10-19 15:40:45 +0200

    better handling of statistics in main page

--- qsdb-functions.js
+++ qsdb-functions.js
@@ -5412,23 +5412,28 @@ function accession_search_parse_accessions(accessions, synchronous){
 
 
 function request_load_proteins(data, check, post_load){
-    if (typeof post_load === "undefined") post_load = false;
-    for (var node_id in data){
-        var p_id = data[node_id]["i"];
-        var prot = 0;
-        if (p_id in protein_dictionary){
-            prot = protein_dictionary[p_id];
-            prot.update(data[node_id]);
+    
+    
+        if (typeof post_load === "undefined") post_load = false;
+        for (var node_id in data){
+            var p_id = data[node_id]["i"];
+            var prot = 0;
+            if (p_id in protein_dictionary){
+                prot = protein_dictionary[p_id];
+                prot.update(data[node_id]);
+            }
+            else {
+                prot = new Protein(data[node_id], 0);
+                protein_dictionary[prot.id] = prot;
+            }
+            prot.filtering();
+            if (prot.filter_valid && !post_load) basket[prot.id] = prot;
         }
-        else {
-            prot = new Protein(data[node_id], 0);
-            protein_dictionary[prot.id] = prot;
-        }
-        prot.filtering();
-        if (prot.filter_valid && !post_load) basket[prot.id] = prot;
-    }
-    if (check) check_spectra();
-    if (post_load) draw();
+        if (check) check_spectra();
+        if (post_load) draw();
+    
+    
+    
 }
 
 
