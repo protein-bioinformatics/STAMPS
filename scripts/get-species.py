0e0de7bda7be9ad00501382a38a7300ca0539234
dominik.kopczynski <dominik.kopczynski@isas.de>
2019-09-19 09:36:08 +0200

    adding species script

---
+++ scripts/get-species.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import json
+from pymysql import connect, cursors
+
+print("Content-Type: text/html")
+print()
+
+
+conf = {}
+with open("../admin/qsdb.conf", mode="rt") as fl:
+    for line in fl:
+        line = line.strip().strip(" ")
+        if len(line) < 1 or line[0] == "#": continue
+        token = line.split("=")
+        if len(token) < 2: continue
+        conf[token[0].strip(" ")] = token[1].strip(" ")
+
+
+conn = connect(host = conf["mysql_host"], port = int(conf["mysql_port"]), user = conf["mysql_user"], passwd = conf["mysql_passwd"], db = conf["mysql_db"])
+my_cur = conn.cursor()
+
+my_cur.execute('SELECT ncbi, name FROM species;')
+print(json.dumps({row[0]: row[1] for row in my_cur}))
