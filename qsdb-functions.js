fd2acfbde9ce6bc09be429600e9ed0de72a4e9f9
dominik.kopczynski <dominik.kopczynski@isas.de>
2019-10-02 12:33:34 +0200

    full reset when changing host

--- qsdb-functions.js
+++ qsdb-functions.js
@@ -5469,7 +5469,7 @@ function prepare_infobox(prot){
 
 function load_data(reload){
     
-    if (new_host != current_host){
+    if (new_host != "" && new_host != current_host){
         if (confirm("Warning: you are changing the host, all selected proteins will be discarded. Do you want to continue?")){
             current_host = new_host;
             basket = {};
